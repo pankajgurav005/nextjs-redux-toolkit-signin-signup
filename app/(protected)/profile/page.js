79534d9bc86c9f29c55a0e6e5860e9bc0b7baa86
Pankaj Gurav <pankajgurav.work@gmail.com>
2024-02-07 11:41:07 +0530

    Profile page enhancement

--- app/(protected)/profile/page.js
+++ app/(protected)/profile/page.js
@@ -15,21 +15,34 @@ export default async function Profile() {
   }
   return(
     <>
-      <div className="bg-white p-8 rounded shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-4">User Profile</h1>
-        <div className="mb-4 flex gap-4 items-baseline">
-          <label className="block text-gray-700 text-sm font-bold mb-2">First Name:</label>
-          <p className="text-gray-800 capitalize">{user.first_name}</p>
+      <section style={{ height: '90vh' }} className="flex font-medium items-center justify-center">
+      <section className="w-64 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg">
+        <div className="flex items-center justify-between">
+          <span className="text-gray-400 text-sm">2d ago</span>
+          <span className="text-emerald-400">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+            </svg>
+          </span>
         </div>
-        <div className="mb-4 flex gap-4 items-baseline">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Last Name:</label>
-          <p className="text-gray-800 capitalize">{user.last_name}</p>
+        <div className="mt-6 w-fit mx-auto">
+          <img src="https://i.ibb.co/DbqW1Jb/6388000.png" className="rounded-full w-28" alt="profile picture" />
         </div>
-        <div className="mb-4 flex gap-4 items-baseline">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-          <p className="text-gray-800">{user.email}</p>
+        <div className="mt-8 ">
+          <h2 className="text-white font-bold text-2xl tracking-wide capitalize">{user.first_name} {user.last_name}</h2>
         </div>
-      </div>
+        <p className="text-emerald-400 font-semibold mt-2.5">
+          {user.email}
+        </p>
+        <div className="h-1 w-full bg-black mt-8 rounded-full">
+          <div className="h-1 rounded-full w-2/5 bg-yellow-500 "></div>
+        </div>
+        <div className="mt-3 text-white text-sm">
+          <span className="text-gray-400 font-semibold">Storage:</span>
+          <span>40%</span>
+        </div>
+      </section>
+    </section>
     </>
   )
 }
